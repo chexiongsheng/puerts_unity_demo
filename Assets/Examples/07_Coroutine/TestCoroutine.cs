bfd1ea4251a3859a4fe9639ca6c45c0c47778578
layerpsr <45587825+layerpsr@users.noreply.github.com>
2020-09-08 18:00:26 +0800

    创建Unity协程

---
+++ Assets/Examples/07_Coroutine/TestCoroutine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Puerts;
+using UnityEngine;
+
+namespace PuertsTest
+{
+    public class TestCoroutine : MonoBehaviour
+    {
+        public delegate void Init(TestCoroutine monoBehaviour);
+
+        static JsEnv jsEnv;
+
+
+        void Awake()
+        {
+            if (jsEnv == null)
+            {
+                jsEnv = new JsEnv();
+                jsEnv.UsingFunc<bool>();
+            }
+
+            var init = jsEnv.Eval<Init>("const m = require('coroutine'); m.init;");
+            if (init != null)
+                init(this);
+        }
+
+        void OnDestroy()
+        {
+            jsEnv?.Dispose();
+        }
+    }
+}
---
+++ Assets/Examples/07_Coroutine/TestUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TestUtil
+{
+    public static IEnumerator Generator(Func<object> next, Func<bool> done)
+    {
+        var _done = false;
+        while (!_done)
+        {
+            yield return next();
+            _done = done();
+        }
+    }
+}
