d57aa983e3027dd2d114fdb57249631fe3a5eb39
zombieyang <zombieyang@tencent.com>
2021-09-21 21:29:31 +0800

    waitForDebugger

--- Assets/Puerts/Src/JsEnv.cs
+++ Assets/Puerts/Src/JsEnv.cs
@@ -585,6 +585,11 @@ namespace Puerts
 
         public void WaitDebugger()
         {
+            if (mode == JsEnvMode.Node)
+            {
+                Eval("require('inspector').waitForDebugger()");
+                return;
+            }
 #if THREAD_SAFE
             lock(this) {
 #endif
