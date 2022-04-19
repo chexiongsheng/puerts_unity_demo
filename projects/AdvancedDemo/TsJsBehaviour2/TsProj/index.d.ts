78c86f4802929755cf1b59c9911221a468d2f19e
DOSexample <saytanito@gmail.com>
2022-04-19 17:22:27 +0700

    AdvancedDemo/TsJsBehaviour2
    AdvancedDemo/TsJsBehaviour2

---
+++ projects/AdvancedDemo/TsJsBehaviour2/TsProj/index.d.ts
@@ -0,0 +1,11 @@
+namespace PuertsTest {
+        class JsBehaviour2 extends UnityEngine.MonoBehaviour
+        {
+            public JsStart : System.Action
+            public JsFixedUpdate : System.Action
+            public JsUpdate : System.Action
+            public JsLateUpdate : System.Action
+            public JsOnDestroy : System.Action
+            public constructor ()
+        }
+}
