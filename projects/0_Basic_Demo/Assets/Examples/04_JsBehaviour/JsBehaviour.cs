a5072b322b3bb365bb0a83cb7ea49a93ab668241
zombieyang(杨浩) <zombieyang@tencent.com>
2023-07-14 12:13:20 +0800

    ExecuteModule usage example optimize

--- projects/0_Basic_Demo/Assets/Examples/04_JsBehaviour/JsBehaviour.cs
+++ projects/0_Basic_Demo/Assets/Examples/04_JsBehaviour/JsBehaviour.cs
@@ -23,7 +23,7 @@ namespace PuertsTest
         {
             if (jsEnv == null) jsEnv = new JsEnv(new DefaultLoader(), 9229);
             var varname = "m_" + Time.frameCount;
-            var init = jsEnv.ExecuteModule<ModuleInit>(ModuleName, "init");
+            var init = jsEnv.ExecuteModule(ModuleName).Get<ModuleInit>("init");
 
             if (init != null) init(this);
 
